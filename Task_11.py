c2b36d3190d7c9f60b14127e22db1d340bf76a64
Mincer1 <ivanparshukov0@gmail.com>
2025-07-25 11:41:06 +0500

    Task_11 1-2

---
+++ Task_11.py
@@ -0,0 +1,25 @@
+import math
+
+#1
+# price_eur = float(input("Введите цену в евро: "))
+# eur_usd = 1.17
+# usd_rub = 79.51
+# convector = round(price_eur * eur_usd * usd_rub,2)
+# print(f"Цена в рублях {convector}")
+
+#2
+# count_num = int(input("Введите кол-во чисел: "))
+# for x in range(count_num):
+#     num = float(input("Введите число: "))
+    
+#     if num > 0:
+#         num = math.ceil(num)
+#         print(f"x = {num} log(x) = {math.log(num)} ")
+#     else:
+#         num = math.floor(num)
+#         print(f"x = {num} exp(x) = {math.exp(num)}")
+#3
+#4
+#5
+#6
+#7
