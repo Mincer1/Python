fb27a0646d8eb287f3bce225d861ef2312610f0b
Ivan <ivan.parshukov0@gmail.com>
2025-08-04 20:47:00 +0500

    Pc Task_14

---
+++ SkillBox_Python/Task_14.py
@@ -0,0 +1,37 @@
+#1.1
+# numbers_list = [3,7,5]
+
+# while True:
+
+#  number = int(input('Новое число: '))
+
+#  numbers_list.append(number)
+
+#  print('Текущий список чисел:', numbers_list)
+
+#  for i in numbers_list:
+
+#    print(i ** 2, i ** 3, i ** 4)
+
+#  print()
+
+#1.2
+# numbers = []
+# for i in range(101):
+#     numbers.append(i)
+# print(numbers)
+
+#1.3
+# count_id = int(input("сколько сотрудников?: "))
+# employee_id = []
+
+# for _ in range(count_id):
+#     id = int(input("id сотрудника: "))
+#     employee_id.append(id)
+
+# search_id = int(input("Какой id ищем?: "))
+
+# if search_id in employee_id:
+#     print("Сотрудник работает!")
+# else:
+#     print("не работает")
